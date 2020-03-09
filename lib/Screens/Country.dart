b639f0ec7591f59bd3651ebfb95c88cecd10a6b4
Monel DOSSOU <monelyehoun@gmail.com>
2020-03-09 16:00:20 +0100

    Install du plugin flip_card

--- lib/Screens/Country.dart
+++ lib/Screens/Country.dart
@@ -12,6 +12,42 @@ class Country extends StatelessWidget {
         backgroundColor: Colors.indigo,
         title: Text(country['name']),
       ),
+      body: Container(
+        padding: EdgeInsets.all(10),
+        child: GridView(
+          gridDelegate:
+              SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+          children: <Widget>[
+            CountryCard(title: 'Capitale'),
+            CountryCard(title: 'Population'),
+            CountryCard(title: 'Drapeau'),
+            CountryCard(title: 'Monnaie'),
+            CountryCard(title: 'Continent'),
+            CountryCard(title: 'Afficher la carte'),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CountryCard extends StatelessWidget {
+  final String title;
+  const CountryCard({
+    Key key,
+    this.title,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 10,
+      child: Center(
+        child: Text(
+          title,
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+      ),
     );
   }
 }
