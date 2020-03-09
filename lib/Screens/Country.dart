1b98beaa77dfdbe319dc004d2f0ae8936116cc6d
Monel DOSSOU <monelyehoun@gmail.com>
2020-03-09 17:27:16 +0100

    Affichage des détails de chaque pays

--- lib/Screens/Country.dart
+++ lib/Screens/Country.dart
@@ -1,4 +1,6 @@
+import 'package:flip_card/flip_card.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class Country extends StatelessWidget {
   final Map country;
@@ -18,13 +20,71 @@ class Country extends StatelessWidget {
           gridDelegate:
               SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
           children: <Widget>[
-            CountryCard(title: 'Capitale'),
-            CountryCard(title: 'Population'),
-            CountryCard(title: 'Drapeau'),
-            CountryCard(title: 'Monnaie'),
-            CountryCard(title: 'Continent'),
+            FlipCard(
+              direction: FlipDirection.VERTICAL,
+              front: CountryCard(title: 'Capitale'),
+              back: CountryDetailCard(
+                title: country['capital'],
+              ),
+            ),
+            FlipCard(
+              direction: FlipDirection.VERTICAL,
+              front: CountryCard(title: 'Population'),
+              back: CountryDetailCard(
+                title: country['population'].toString(),
+              ),
+            ),
+            FlipCard(
+              direction: FlipDirection.VERTICAL,
+              front: CountryCard(title: 'Drapeau'),
+              back: Card(
+                elevation: 10,
+                color: Colors.white,
+                child: Center(
+                    child: SvgPicture.network(
+                  country['flag'],
+                  width: 200,
+                )),
+              ),
+            ),
+            FlipCard(
+              direction: FlipDirection.VERTICAL,
+              front: CountryCard(title: 'Monnaie'),
+              back: CountryDetailCard(
+                title: country['currencies'][0]['name'],
+              ),
+            ),
+            FlipCard(
+              direction: FlipDirection.VERTICAL,
+              front: CountryCard(title: 'Continent'),
+              back: CountryDetailCard(
+                title: country['region'],
+              ),
+            ),
             CountryCard(title: 'Afficher la carte'),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class CountryDetailCard extends StatelessWidget {
+  final String title;
+  const CountryDetailCard({
+    Key key,
+    this.title,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 10,
+      color: Colors.indigoAccent,
+      child: Center(
+        child: Text(
+          title,
+          style: TextStyle(fontSize: 18, color: Colors.white),
         ),
       ),
     );
